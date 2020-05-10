48ef39eed2e2c1bce72d61c7fd295e7806ba3f94
honyax <honyax@gmail.com>
2020-05-10 17:07:38 +0900

    フォントファイルを追加、文字表示機能を追加

--- bootpack.c
+++ bootpack.c
@@ -8,6 +8,8 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfont8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -38,6 +40,10 @@ void HariMain(void)
 	
 	init_palette();
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+	
+	putfont8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "ABC 123");
+	putfont8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "HonyaOS.");
+	putfont8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "HonyaOS.");
 	
 	for (;;) {
 		io_hlt();
@@ -114,4 +120,31 @@ void init_screen(char *vram, int x , int y)
 	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
 	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
 	return;
+}
+
+void putfont8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for (; *s != 0x00; s++) {
+		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+		x += 8;
+	}
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p, d /* data */;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; }
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
 }
