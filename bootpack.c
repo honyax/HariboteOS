5ebda6181c9d3e0e91d283a2eaedab175456c51e
honyax <honyax@gmail.com>
2020-05-29 01:50:28 +0900

    harib06d メモリ管理の仕組みを導入

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,38 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+#define MEMMAN_FREES		4090		// これで約32KB
+#define MEMMAN_ADDR			0x003c0000
+
+// 空き情報
+struct FREEINFO {
+	unsigned int addr;
+	unsigned int size;
+};
+
+// メモリ管理
+struct MEMMAN {
+	int frees;
+	int maxfrees;
+	int lostsize;
+	int losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
 unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
+	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
+	struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
 
 	int xmin = 0;
 	int ymin = 0;
@@ -27,6 +51,10 @@ void HariMain(void)
 	
 	init_keyboard();
 	enable_mouse(&mdec);
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000);	// 0x00001000 - 0x0009efff
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -37,8 +65,8 @@ void HariMain(void)
 	sprintf(s, "(%3d, %3d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
-	sprintf(s, "memory:%dMB", i);
+	sprintf(s, "memory %dMB   free : %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
+	
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 	
 	for (;;) {
@@ -128,4 +156,109 @@ unsigned int memtest(unsigned int start, unsigned int end)
 	}
 
 	return i;
+}
+
+void memman_init(struct MEMMAN *man)
+{
+	man->frees = 0;		// 空き情報の個数
+	man->maxfrees = 0;	// 状況観察用：freesの最大値
+	man->lostsize = 0;	// 解放に失敗した合計サイズ
+	man->losts = 0;		// 解放に失敗した回数
+}
+
+// 空きサイズの合計を報告
+unsigned int memman_total(struct MEMMAN *man)
+{
+	unsigned int i, t = 0;
+	for (i = 0; i < man->frees; i++) {
+		t += man->free[i].size;
+	}
+	return t;
+}
+
+// 確保
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int i, a;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			// 十分な広さのある空きを発見
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size == 0) {
+				// free[i]がなくなったので前へつめる
+				man->frees--;
+				for (; i < man->frees; i++) {
+					man->free[i] = man->free[i + 1];	// 構造体の代入
+				}
+			}
+			return a;
+		}
+	}
+	return 0;	// 空きがない
+}
+
+// 解放
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+	int i, j;
+	int mergefront = 0;
+	int mergeback = 0;
+	// まとめやすさを考えると、free[]がaddr順に並んでいる方が良い
+	// だからまず、どこに入れるべきかを決める
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) {
+			break;
+		}
+	}
+
+	// free[i - 1].addr < addr < free[i].addr
+	// 前にまとめられるか？
+	if (i > 0 && man->free[i - 1].addr + man->free[i - 1].size == addr) {
+		mergefront = 1;
+	}
+	// 後ろにまとめられるか？
+	if (i < man->frees && addr + size == man->free[i].addr) {
+		mergeback = 1;
+	}
+
+	if (mergefront == 1) {
+		// 前とまとめる
+		man->free[i - 1].size += size;
+
+		if (mergeback == 1) {
+			// さらに後ろとまとめる
+			man->free[i - 1].size += man->free[i].size;
+
+			// i 以降を前につめる
+			man->frees--;
+			for (; i < man->frees; i++) {
+				man->free[i] = man->free[i + 1];	// 構造体の代入
+			}
+		}
+		return 0;		// 成功終了
+	} else if (mergeback == 1) {
+		// 後ろとまとめる
+		man->free[i].addr = addr;
+		man->free[i].size += size;
+		return 0;		// 成功終了
+	} else if (man->frees < MEMMAN_FREES) {
+		// 前とも後ろともまとめられないので、i 以降を後ろにズラす
+		for (j = man->frees; j > i; j--) {
+			man->free[j] = man->free[j -  1];
+		}
+		man->frees++;
+		if (man->maxfrees < man->frees) {
+			man->maxfrees = man->frees;		// 最大値を更新
+		}
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0;		// 成功終了
+	} else {
+		// 前とも後ろともまとめられない上に後ろにズラせない。ひとまずロストさせる
+		man->losts++;
+		man->lostsize += size;
+		return -1;		// 失敗終了
+	}
 }
