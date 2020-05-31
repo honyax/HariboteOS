512b5f6838567f4a30744eea01ae7eefa9b911e9
honyax <honyax@gmail.com>
2020-05-31 12:41:15 +0900

    harib07c 再描画する領域をシートの移動前、移動後のみにしてパフォーマンス向上

--- bootpack.c
+++ bootpack.c
@@ -55,7 +55,7 @@ void HariMain(void)
 
 	sprintf(s, "memory %dMB   free : %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
-	sheet_refresh(shtctl);
+	sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 	
 	for (;;) {
 		io_cli();
@@ -68,7 +68,7 @@ void HariMain(void)
 				sprintf(s, "%02X", i);
 				boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
 				putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-				sheet_refresh(shtctl);
+				sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
 			} else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
@@ -86,6 +86,7 @@ void HariMain(void)
 					}
 					boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 
 					// マウスカーソルの移動
 					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15);	// マウスを消す
@@ -99,7 +100,8 @@ void HariMain(void)
 					sprintf(s, "(%3d, %3d)", mx, my);
 					boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);	// 前回の座標を消す
 					putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);	// 座標を書く
-					sheet_slide(shtctl, sht_mouse, mx, my);	// sheet_refreshを含む
+					sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+					sheet_slide(shtctl, sht_mouse, mx, my);
 				}
 			}
 		}
