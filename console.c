7d70fa40750461ef4c4621cfcf1402da69ae6f9f
honyax <honyax@gmail.com>
2020-06-12 02:26:34 +0900

    harib18g アプリがOS用のセグメントを使えないようにした。アクセス権に0x60を渡すとx86のCPUが自動的に対応してくれる

--- console.c
+++ console.c
@@ -261,6 +261,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
 	char name[18];
 	char *p, *q;
+	struct TASK *task = task_now();
 	int i;
 
 	// コマンドラインからファイル名を生成
@@ -291,8 +292,8 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 		q = (char *) memman_alloc_4k(memman, 64 * 1024);
 		*((int *) 0xfe8) = (int) p;
 		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));
-		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER);
-		set_segmdesc(gdt + 1004, 64 * 1024 - 1,   (int) q, AR_DATA32_RW);
+		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER + 0x60);
+		set_segmdesc(gdt + 1004, 64 * 1024 - 1,   (int) q, AR_DATA32_RW + 0x60);
 		if (finfo->size >= 8 && strncmp(p + 4, "Hari", 4) == 0) {
 			p[0] = 0xe8;
 			p[1] = 0x16;
@@ -301,7 +302,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 			p[4] = 0x00;
 			p[5] = 0xcb;
 		}
-		start_app(0, 1003 * 8, 64 * 1024, 1004 * 8);
+		start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
 		memman_free_4k(memman, (int) p, finfo->size);
 		memman_free_4k(memman, (int) q, 64 * 1024);
 		cons_newline(cons);
@@ -312,9 +313,10 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 	}
 }
 
-void hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
+int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
 {
 	int cs_base = *((int *) 0xfe8);
+	struct TASK *task = task_now();
 	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
 	switch (edx) {
 		case 1:
@@ -326,15 +328,19 @@ void hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		case 3:
 			cons_putstr1(cons, (char *) ebx + cs_base, ecx);
 			break;
+		case 4:
+			return &(task->tss.esp0);
 		default:
 			break;
 	}
+	return 0;
 }
 
-int inthandler0d(int *esp)
+int *inthandler0d(int *esp)
 {
 	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
+	struct TASK *task = task_now();
 	cons_putstr0(cons, "\nINT 0D :\n General Protected Exception.\n");
 	// 異常終了させる
-	return 1;
+	return &(task->tss.esp0);
 }
