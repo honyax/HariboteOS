e4aa8620dbcc7e185ed3a6f7d06c8d94a2808467
honyax <honyax@gmail.com>
2020-06-16 00:04:40 +0900

    harib20e ウィンドウを閉じるAPIを実装

--- console.c
+++ console.c
@@ -398,6 +398,9 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 				sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
 			}
 			break;
+		case 14:
+			sheet_free((struct SHEET *) ebx);
+			break;
 		default:
 			break;
 	}
