b7cf9b428548ffb7ba47472f3c958328a6a298d2
honyax <honyax@gmail.com>
2020-06-20 03:16:25 +0900

    harib23d ウィンドウ移動の高速化（４）

--- bootpack.c
+++ bootpack.c
@@ -17,6 +17,7 @@ void HariMain(void)
 	int keycmd_buf[32];
 	int *cons_fifo[2];
 	int mx, my, i;
+	int new_mx = -1, new_my = 0, new_wx = 0x7fffffff, new_wy = 0;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
@@ -143,8 +144,19 @@ void HariMain(void)
 		}
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
-			task_sleep(task_a);
-			io_sti();
+			// FIFOがからっぽになったので、保留している描画があれば実行する
+			if (new_mx >= 0) {
+				io_sti();
+				sheet_slide(sht_mouse, new_mx, new_my);
+				new_mx = -1;
+			} else if (new_wx != 0x7fffffff) {
+				io_sti();
+				sheet_slide(sht, new_wx, new_wy);
+				new_wx = 0x7fffffff;
+			} else {
+				task_sleep(task_a);
+				io_sti();
+			}
 		} else {
 			i = fifo32_get(&fifo);
 			io_sti();
@@ -256,7 +268,8 @@ void HariMain(void)
 					if (my < ymin) { my = ymin; }
 					if (mx > xmax) { mx = xmax; }
 					if (my > ymax) { my = ymax; }
-					sheet_slide(sht_mouse, mx, my);
+					new_mx = mx;
+					new_my = my;
 					if ((mdec.btn & 0x01) != 0) {
 						// 左クリック
 						if (mmx < 0) {
@@ -279,6 +292,7 @@ void HariMain(void)
 											mmx = mx;
 											mmy = my;
 											mmx2 = sht->vx0;
+											new_wy = sht->vy0;
 										}
 										if (sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19) {
 											// 「×」ボタンクリック
@@ -302,7 +316,8 @@ void HariMain(void)
 							// マウスの移動量を計算
 							x = mx - mmx;
 							y = my - mmy;
-							sheet_slide(sht, (mmx2 + x + 2) & ~3, sht->vy0 + y);
+							new_wx = (mmx2 + x + 2) & ~3;
+							new_wy = new_wy + y;
 							// 移動後の座標に更新
 							mmy = my;
 						}
@@ -310,6 +325,10 @@ void HariMain(void)
 						// 左クリックしていない
 						// 通常モードへ
 						mmx = -1;
+						if (new_wx != 0x7fffffff) {
+							sheet_slide(sht, new_wx, new_wy);	// 一度確定させる
+							new_wx = 0x7fffffff;
+						}
 					}
 				}
 			}
