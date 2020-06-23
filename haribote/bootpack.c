8dd3d15cd688b162955c18b4d5b58b76fff4bc5c
honyax <honyax@gmail.com>
2020-06-24 01:55:16 +0900

    harib25e 日本語表示（１）（半角カナを表示するアプリを実装）

--- haribote/bootpack.c
+++ haribote/bootpack.c
@@ -53,6 +53,10 @@ void HariMain(void)
 	struct SHEET *sht = 0;
 	struct SHEET *key_win;
 	struct SHEET *sht2;
+	int *fat;
+	unsigned char *nihongo;
+	struct FILEINFO *finfo;
+	extern char hankaku[4096];
 
 	int xmin = 0;
 	int ymin = 0;
@@ -84,6 +88,7 @@ void HariMain(void)
 	fifo.task = task_a;
 	task_run(task_a, 1, 2);
 	*((int *) 0x0fe4) = (int) shtctl;
+	task_a->langmode = 0;
 
 	// sht_back
 	sht_back	= sheet_alloc(shtctl);
@@ -112,6 +117,26 @@ void HariMain(void)
 	// 最初にキーボード状態との食い違いがないように、設定しておくことにする
 	fifo32_put(&keycmd, KEYCMD_LED);
 	fifo32_put(&keycmd, key_leds);
+
+	// nihongo.fntの読み込み
+	nihongo = (unsigned char *) memman_alloc_4k(memman, 16 * 256 + 32 * 94 * 47);
+	fat = (int *) memman_alloc_4k(memman, 4 * 2880);
+	file_readfat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
+	finfo = file_search("nihongo.fnt", (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	if (finfo != 0) {
+		file_loadfile(finfo->clustno, finfo->size, nihongo, fat, (char *) (ADR_DISKIMG + 0x003e00));
+	} else {
+		for (i = 0; i < 16 * 256; i++) {
+			// フォントがなかったので半角部分をコピー
+			nihongo[i] = hankaku[i];
+		}
+		for (i = 16 * 256; i < 16 * 256 + 32 * 94 * 47; i++) {
+			// フォントがなかったので全角部分を 0xff で埋め尽くす
+			nihongo[i] = 0xff;
+		}
+	}
+	*((int *) 0x0fe8) = (int) nihongo;
+	memman_free_4k(memman, (int) fat, 4 * 2880);
 	
 	for (;;) {
 		if (fifo32_status(&keycmd) > 0 && keycmd_wait < 0) {
--- haribote/file.c
+++ haribote/file.c
@@ -5,7 +5,7 @@ void file_readfat(int *fat, unsigned char *img)
 {
 	int i, j = 0;
 	for (i = 0; i < 2880; i += 2) {
-		fat[i + 0] = (img[j + 0]      | img[j + i] << 8) & 0xfff;
+		fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;
 		fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;
 		j += 3;
 	}
